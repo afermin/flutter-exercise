9992e162a838ec44b158081665e1820a30a7d1c2
Alexander Fermin <alexfer06@gmail.com>
2022-10-09 17:20:52 -0500

    Adding ShortenUseCase

--- lib/domain/usecases/shorten_link_use_case.dart
+++ lib/domain/usecases/shorten_link_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:nubank_exercise/domain/models/failure.dart';
 import 'package:nubank_exercise/domain/usecases/use_case.dart';
 
@@ -6,6 +7,7 @@ import '../entities/link.dart';
 
 abstract class ShortenLinkUseCase extends UseCase<Link, String> {}
 
+@injectable
 class ShortenLinkUseCaseImpl implements ShortenLinkUseCase {
   @override
   Future<Either<Failure, Link>> invoke(String params) {
