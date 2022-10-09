f457539c1e3b38dc9a2d9e3b1633679125ed85f7
Alexander Fermin <alexfer06@gmail.com>
2022-10-09 17:19:52 -0500

    Adding Failure model

--- lib/domain/models/failure.dart
+++ lib/domain/models/failure.dart
@@ -1,7 +1,12 @@
-class Failure {
-  Failure({
+import 'package:equatable/equatable.dart';
+
+class Failure extends Equatable {
+  const Failure({
     required this.message,
   });
 
   final String message;
+
+  @override
+  List<Object?> get props => [message];
 }
